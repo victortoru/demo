9c3751d98b0ce8526bb84966d5eae1f13991db73
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-01-13 19:12:07 +0100

    yh

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("De momento no hay na");
     }
 }
