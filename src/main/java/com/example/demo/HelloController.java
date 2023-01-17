7fa0d156d97fe2b4514533f664eec90cfc997a90
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-01-17 20:31:29 +0100

    yh

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    Button boton1;
+    @FXML
+    Button boton2;
+    @FXML
+    Button boton3;
+    @FXML
+    Button boton4;
+    @FXML
+    Button boton5;
+    @FXML
+    Button boton6;
+    @FXML
+    Button boton7;
+    @FXML
+    Button boton8;
+    @FXML
+    Button boton9;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("De momento no hay na");
-    }
+    Button Modo_ordenadores;
+
+    @FXML
+    Button Modo_Humanos;
+
+    @FXML
+    Button Modo_HumanovsComputer;
+
+    @FXML
+    Button Comenzar;
+
 }
+
